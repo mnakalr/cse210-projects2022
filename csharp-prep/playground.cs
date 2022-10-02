e09088859b2f3d315ad9f651dda124a907a9dc76
mnakalr <nak21003@byui.edu>
2022-10-02 10:15:18 +0200

    improved switch player

--- csharp-prep/playground.cs
+++ csharp-prep/playground.cs
@@ -1,33 +1,40 @@
 using System;
-using System.Collections.Generic;
 
-namespace Playgrounds
+namespace solo_prep_2
 {
     class Program
     {
         static void Main(string[] args)
+        
+            
         {
             string currentPlayer = "x";
-            string player1 = ChangePlayers(currentPlayer);
-            Console.WriteLine($"player 1 is: {player1}");
-            string player2 = ChangePlayers(currentPlayer);
-            Console.WriteLine($"player 2 is: {player2}");
-        
-        }
-
-        static string ChangePlayers(string currentPlayer)
-        {
             if (currentPlayer == "x")
             {
-                return "o";
+                currentPlayer = "o";
             }
             else
             {
-                return "x";
+                currentPlayer = "x";
             }
+
+            Console.WriteLine($"player 1 is: {currentPlayer}");
+
+            if (currentPlayer == "x")
+            {
+                currentPlayer = "o";
+            }
+            else
+            {
+                currentPlayer = "x";
+            }
+
+            Console.WriteLine($"player 2 is: {currentPlayer}");
+        
         }
     }
 }
+
 
 
 
